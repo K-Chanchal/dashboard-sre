8d4ff49d5d796b7577a51d355c53186b3800c0d8
K-Chanchal <chanchal.kumari@keysight.com>
2025-12-16 15:17:36 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://qnw5w902f4.execute-api.us-west-2.amazonaws.com/default/api';
 const REFRESH_INTERVAL = 300000; // 5 minutes
 const SERVER_TYPE_ROTATION_INTERVAL = 5000; // 5 seconds
 
